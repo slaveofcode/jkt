b87033bbea8ba54057f423a32e268c0f9fdc8a1f
Aditya <zeandcode@gmail.com>
2018-01-27 02:35:46 +0700

    Added test for class types

--- test/splitter_test.js
+++ test/splitter_test.js
@@ -38,7 +38,45 @@ describe("Splitter for key-value string based on string literal", () => {
       expect(val).to.equal(comparator[key]);
     }
   });
-  it("should be able to splitting for class types");
+  it("should be able to splitting for class types", () => {
+    const splitter = Splitter();
+
+    class Person {}
+    class Friend {}
+
+    const struct = splitter`
+      name: String
+      email: String
+      phone: String
+      friend: ${Friend}
+      age: Number
+      birthday: Date
+      isMarried: Boolean
+      gift: Object
+      hobby: Array
+      skills: Function
+      person: ${Person}
+    `;
+
+    const comparator = {
+      name: types.STRING,
+      email: types.STRING,
+      phone: types.STRING,
+      friend: Friend,
+      age: types.NUMBER,
+      birthday: types.DATE,
+      isMarried: types.BOOLEAN,
+      gift: types.OBJECT,
+      hobby: types.ARRAY,
+      skills: types.FUNCTION,
+      person: Person
+    };
+
+    console.log(struct);
+    for (const [key, val] of Object.entries(struct)) {
+      expect(val).to.equal(comparator[key]);
+    }
+  });
   it("should be able to splitting for function types");
   it("should be able to splitting for static value");
 });
