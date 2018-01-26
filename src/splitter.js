6e59cf5e313c79f16c9557958d51d4234588686c
Aditya <zeandcode@gmail.com>
2018-01-27 02:30:13 +0700

    Implement splitter func

--- src/splitter.js
+++ src/splitter.js
@@ -1,18 +1,43 @@
 "use strict";
 
-const types = require(`./datatypes`);
+const {
+  STRING,
+  ARRAY,
+  BOOLEAN,
+  DATE,
+  FUNCTION,
+  NUMBER,
+  OBJECT
+} = require(`./datatypes`);
 
 const splitter = (strict = false) => {
-  return (strings, bindings) => {
-    // In loop of splitted strings
-    // Remove all the spaces
-    // Remove all new lines chars
-    // Split based on colon (:)
-    // validate the value side
-    // - Check the value is a defined types, Class Obj, Function or static Defined value (string, number, boolean, date, object)
-    // - if strict true (every invalid value should trigger eror exception)
-    // - added the key with value to return
-    // Return
+  return (strings, ...bindings) => {
+    const pairs = {};
+    let bindIdx = 0;
+
+    strings.forEach(stmt => {
+      // Place a new line marker and Removing all spaces
+      const cleaned = stmt.replace(/\r?\n|\r/g, "|").replace(/\s/g, "");
+      const splitLines = cleaned.split("|").filter(itm => itm.length > 0);
+
+      splitLines.forEach(block => {
+        const [key, val] = block.split(":");
+        pairs[key] = val.length > 0 ? val : bindings[bindIdx];
+        if (val.length === 0) bindIdx++;
+      });
+    });
+
+    const pairVals = Object.values(pairs);
+    const countUndefTypes = pairVals.map(
+      typeVal => typeof typeVal === "undefined"
+    ).length;
+
+    if (countUndefTypes > 0 && strict)
+      throw new Error(
+        "Invalid Schema Detected, please define right value types"
+      );
+
+    return pairs;
   };
 };
 
