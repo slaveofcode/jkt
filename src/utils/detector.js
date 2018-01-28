f943e35947413090e05ce6faca4621c51b6caac5
Aditya <zeandcode@gmail.com>
2018-01-29 00:48:22 +0700

    fix comparator function

--- src/utils/detector.js
+++ src/utils/detector.js
@@ -10,19 +10,19 @@ const NULL_V = "[object Null]";
 const DATE_V = "[object Date]";
 const UNDEF_V = "[object Undefined]";
 
-const comparator = (obj, comp) => {
-  return () => Object.prototype.toString.call(obj) === comp;
+const comparator = comp => {
+  return obj => Object.prototype.toString.call(obj) === comp;
 };
 
-const isArray = comparator(obj, ARRAY_V);
-const isObject = comparator(obj, OBJECT_V);
-const isFunction = comparator(obj, FUNC_V);
-const isString = comparator(obj, STRING_V);
-const isNumber = comparator(obj, NUMBER_V);
-const isBoolean = comparator(obj, BOOL_V);
-const isNull = comparator(obj, NULL_V);
-const isDate = comparator(obj, DATE_V);
-const isUndefined = comparator(obj, UNDEF_V);
+const isArray = comparator(ARRAY_V);
+const isObject = comparator(OBJECT_V);
+const isFunction = comparator(FUNC_V);
+const isString = comparator(STRING_V);
+const isNumber = comparator(NUMBER_V);
+const isBoolean = comparator(BOOL_V);
+const isNull = comparator(NULL_V);
+const isDate = comparator(DATE_V);
+const isUndefined = comparator(UNDEF_V);
 
 module.exports = {
   isArray,
