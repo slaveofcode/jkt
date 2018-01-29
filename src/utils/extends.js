41b44d5953eff56653d3d1bac9fa25170070f11c
Aditya <zeandcode@gmail.com>
2018-01-29 11:31:50 +0700

    Solved import

--- src/utils/extends.js
+++ src/utils/extends.js
@@ -1,15 +1,14 @@
 "use strict";
 
 const Splitter = require("../splitter");
-const makeUtils = require("./make");
-const { Inst } = require("../index");
 
 const extendUtil = (baseSchema, strict) => {
   const splitter = Splitter(strict);
   return (childStrings, ...childBindings) => {
+    const makeUtils = require("./make");
+    const { Inst } = require("../index");
     const childSchema = splitter(childStrings, childBindings);
     const newSchema = { ...baseSchema, ...childSchema };
-    console.log(makeUtils)
     return Inst(newSchema, makeUtils(newSchema));
   };
 };
