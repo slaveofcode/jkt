84dbbd51bfa9e7db9127e515654327ec001da23e
Aditya <zeandcode@gmail.com>
2018-01-30 18:33:35 +0700

    prepare test

--- test/struct_test.js
+++ test/struct_test.js
@@ -6,9 +6,13 @@ const jkt = require(`${src}/index`);
 const expect = chai.expect;
 
 describe("Struct", () => {
-  it("should be able to use wit basic operation", () => {
+  it("should be able to extend", () => {
     const schema = struct`
       name: String
     `;
   });
+  it("should trigger error when using reserved words", () => {});
+  it("should be able to parse in-listed values", () => {});
+  it("should be able to parse object values", () => {});
+  it("should be able to parse function values", () => {});
 });
