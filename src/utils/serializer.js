0ca93cbdeed89a18322a5db1364fc76c603a58c8
Aditya <zeandcode@gmail.com>
2018-02-25 01:56:12 +0700

    Added map key extractor and implement on parser also serializer

--- src/utils/serializer.js
+++ src/utils/serializer.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const detector = require("./detector");
+const extractMapKey = require("./mapkey_extractor");
 const {
   STRING,
   ARRAY,
   BOOLEAN,
   DATE,
+  DATE_PLAIN,
   FUNCTION,
   NUMBER,
   OBJECT,
@@ -13,12 +15,13 @@ const {
 } = require("../datatypes");
 
 const isSafeToRelease = typeName =>
-  [STRING, NUMBER, DATE, BOOLEAN].includes(typeName);
+  [STRING, NUMBER, DATE, DATE_PLAIN, BOOLEAN].includes(typeName);
 
-const serializer = {
+const safeSerializer = {
   [STRING]: val => val,
   [NUMBER]: val => val,
   [DATE]: val => (val ? val.toJSON() : null), // ISO-8601 UTC
+  [DATE_PLAIN]: val => (val ? val.toJSON() : null), // ISO-8601 UTC
   [BOOLEAN]: val => val
 };
 
@@ -36,10 +39,14 @@ const serialize = baseSchema => {
     const serializedValues = {};
     Object.keys(baseSchema).forEach(key => {
       const valueType = baseSchema[key];
+
+      const [srcKey, mapKey] = extractMapKey(key);
+      if (mapKey !== null) key = mapKey;
+
       const value = parsedValues[key];
       if (!detector.isUndefined(value))
         if (isSafeToRelease(valueType)) {
-          serializedValues[key] = serializer[valueType](value);
+          serializedValues[key] = safeSerializer[valueType](value);
         } else {
           const purifiedVal = purified(value);
           if (!detector.isUndefined(purifiedVal))
